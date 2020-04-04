e9ef3bfd2ad9e297ee70c0c4581ee88862ba2af4
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2020-04-04 12:27:05 +0100

    Add project files.

---
+++ CSharpLuaTest/Program.cs
@@ -0,0 +1,170 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Horology;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+using MSI = MoonSharp.Interpreter;
+
+namespace CSharpLuaTest
+{
+    [MemoryDiagnoser]
+    public class Program
+    {
+        readonly NLua.Lua _luaState = new NLua.Lua();
+        readonly NLua.Lua _luaStateFunction = new NLua.Lua();
+        readonly NLua.LuaFunction _luaFunction;
+
+        readonly MSI.Script _msScript = new MSI.Script();
+        readonly object _msFunction;
+
+        readonly NLua.Lua _luaStateCallback;
+        readonly NLua.LuaFunction _luaFunctionCallback;
+
+        public Program()
+        {
+            _luaStateFunction.DoString(_factorialFunction);
+            _luaFunction = _luaStateFunction.GetFunction("fact");
+
+            _msScript.DoString(_factorialFunction);
+            _msFunction = _msScript.Globals["fact"];
+
+            // callbacks
+            _luaStateCallback = new NLua.Lua();
+            _luaStateCallback.LoadCLRPackage();
+            _luaStateCallback.DoString(@"
+                import ('CSharpLuaTest')
+            ");
+            // call the static c# function -- don't need to wrap it inside a "real" lua function
+            _luaFunctionCallback = _luaStateCallback.GetFunction("Program.Factorial");
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Run();
+
+            BenchmarkRunner.Run<Program>(
+                DefaultConfig.Instance.With(
+                    Job.Default
+                        .WithIterationCount(10)
+                        .WithIterationTime(TimeInterval.FromMilliseconds(100))
+                        ));
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(MoonSharpTest());
+            Console.WriteLine(NLuaTestNew());
+            Console.WriteLine(NLuaTestKeep());
+            Console.WriteLine(NLuaTestFunctionEval());
+            Console.WriteLine(CSharpFactorial(15));
+            Console.WriteLine(MoonSharpTestFunction(15));
+            Console.WriteLine(NLuaTestFunctionCall(15));
+            Console.WriteLine(NLuaTestFunctionCallback(15));
+        }
+
+        static string _factorialScript = @"    
+        -- defines a factorial function
+        function fact (n)
+            if (n == 0) then
+                return 1
+            else
+                return n*fact(n - 1)
+            end
+        end
+
+        return fact(5)";
+
+        static string _factorialFunction = @"    
+        -- defines a factorial function
+        function fact (n)
+            if (n == 0) then
+                return 1
+            else
+                return n*fact(n - 1)
+            end
+        end";
+
+
+        [Benchmark]
+        public long MoonSharpTest()
+        {
+            var res = MoonSharp.Interpreter.Script.RunString(_factorialScript);
+            return (long)res.Number;
+        }
+
+        [Benchmark]
+        public long NLuaTestNew()
+        {
+            using var state = new NLua.Lua();
+            var res = state.DoString(_factorialScript);
+            return (long)res[0];
+        }
+
+        [Benchmark]
+        public long NLuaTestKeep()
+        {
+            var res = _luaState.DoString(_factorialScript);
+            return (long)res[0];
+        }
+
+
+        [Benchmark]
+        public long NLuaTestFunctionEval()
+        {
+            var res = _luaStateFunction.DoString("return fact(5)");
+            return (long)res[0];
+        }
+
+        public static long Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+
+        [Benchmark]
+        [Arguments(1)]
+        [Arguments(5)]
+        [Arguments(10)]
+        [Arguments(15)]
+        public long CSharpFactorial(int n)
+        {
+            return Factorial(n);
+        }
+
+        [Benchmark]
+        [Arguments(1)]
+        [Arguments(5)]
+        [Arguments(10)]
+        [Arguments(15)]
+        public long NLuaTestFunctionCall(int n)
+        {
+            return (long)_luaFunction.Call(n)[0];
+        }
+
+        [Benchmark]
+        [Arguments(1)]
+        [Arguments(5)]
+        [Arguments(10)]
+        [Arguments(15)]
+        public long MoonSharpTestFunction(int n)
+        {
+            var res = _msScript.Call(_msFunction, n);
+            return (long)res.Number;
+        }
+
+        [Benchmark]
+        [Arguments(1)]
+        [Arguments(5)]
+        [Arguments(10)]
+        [Arguments(15)]
+        public long NLuaTestFunctionCallback(int n)
+        {
+            return (long)_luaFunctionCallback.Call(n)[0];
+        }
+    }
+}
---
+++ CSharpLuaTest/VectorTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpLuaTest
+{
+
+
+    public class VectorTests
+    {
+    }
+}
